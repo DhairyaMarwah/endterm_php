604c242a103f216c6acbc86bb486b4c3193fa58a
dhairyamarwah <dhairyamarwah01@gmail.com>
2023-06-25 22:12:09 +0530

    update

---
+++ FileUpload/index.php
@@ -0,0 +1,30 @@
+<?php
+   if(isset($_FILES['image'])){
+      $errors= array();
+      $file_name = $_FILES['image']['name'];
+      $file_size =$_FILES['image']['size'];
+      $file_tmp =$_FILES['image']['tmp_name'];
+    
+      
+      if($file_size > 2097152){
+         $errors[]='File size must be excately 2 MB';
+      }
+      
+      if(empty($errors)==true){
+         move_uploaded_file($file_tmp,$file_name);
+         echo "Success";
+      }else{
+         print_r($errors);
+      }
+   }
+?>
+<html>
+   <body>
+      
+      <form action="upload_file.php" method="POST" enctype="multipart/form-data">
+         <input type="file" name="image" />
+         <input type="submit"/>
+      </form>
+      
+   </body>
+</html>
